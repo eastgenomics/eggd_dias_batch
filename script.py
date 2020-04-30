84cac7be056d58c10dcf497d22030fb5c8108c48
Matt Garner <matt.j.garner@gmail.com>
2020-04-30 09:00:17 +0100

    Makes a dx output dir, makes a batch file, and runs the batch

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import datetime
 import subprocess
+import uuid
+import os
 
 def get_date():
     date = datetime.datetime.now()
@@ -87,12 +89,69 @@ def get_workflow_stage_info(workflow_id):
 def make_app_out_dirs(workflow_stage_info, workflow_id, workflow_output_dir):
     for stage, stage_info in sorted(workflow_stage_info.items()):
         app_out_dir = "{workflow_output_dir}{app_name}".format(workflow_output_dir=workflow_output_dir, app_name=stage_info["app_name"])
-        command = "dx mkdir {app_out_dir}".format(app_out_dir=app_out_dir)
+        command = "dx mkdir -p {app_out_dir}".format(app_out_dir=app_out_dir)  # -p so no error if multiples of same app try to make multiple dirs e.g. fastqc
         subprocess.check_output(command, shell=True)
     return True
 
 
-workflow_id         = "workflow-FpG0K7Q4yjFkkf05P6xJqZbq"
+def make_dias_batch_file():
+    # uuids for temp files to prevent collisions during parallel runs
+    temp_uuid = str(uuid.uuid4())
+    initial_tsv = temp_uuid+"ini.tsv"
+    temp_tsv = temp_uuid+".tmp.tsv"
+    intermediate_tsv = temp_uuid+".int.tsv"
+    final_tsv = temp_uuid+".final.tsv"
+    command = """
+    dx generate_batch_inputs \
+    -istage-Fk9p4Kj4yBGfpvQf85fQXJq5.reads_fastqgzs='(.*)_S(.*)_L(.*)\d*[13579]_R1(.*)' \
+    -istage-Fk9p4Kj4yBGfpvQf85fQXJq5.reads_fastqgzsB='(.*)_S(.*)_L(.*)\d[02468]_R1(.*)' \
+    -istage-Fk9p4Kj4yBGfpvQf85fQXJq5.reads2_fastqgzs='(.*)_S(.*)_L(.*)\d[13579]_R2(.*)' \
+    -istage-Fk9p4Kj4yBGfpvQf85fQXJq5.reads2_fastqgzsB='(.*)_S(.*)_L(.*)\d[02468]_R2(.*)' \
+    -istage-FpGkFJj433GxvX376JyVxKpG.reads='(.*)_S(.*)_L(.*)\d*[13579]_R1(.*)' \
+    -istage-FpGkFK0433Gy74J9PYJKV42y.reads='(.*)_S(.*)_L(.*)\d[02468]_R1(.*)' \
+    -istage-FpGkFK0433Gy74J9PYJKV42z.reads='(.*)_S(.*)_L(.*)\d[13579]_R2(.*)' \
+    -istage-FpGkF3j433GZg9QQ6X82Gj9V.reads='(.*)_S(.*)_L(.*)\d[02468]_R2(.*)'; \
+    \
+    head -n 1 dx_batch.0000.tsv \
+    > {temp_tsv} && \
+    tail -n +2 dx_batch.0000.tsv | \
+    awk '{{ $10 = \"[\"$10; print }}'  | awk '{{ $11 = $11\"]\"; print }}' |  \
+    awk '{{ $12 = \"[\"$12; print }}'  | awk '{{ $13 = $13\"]\"; print }}' \
+    >> {temp_tsv}; \
+    tr -d '\r' < {temp_tsv} > {intermediate_tsv}; \
+    rm {temp_tsv}
+    \
+    head -n 1 {intermediate_tsv} | \
+    awk -F "\t" '{{ print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$12"\t"$14"\t"$15"\t"$16"\t"$17"\tstage-Fk9p4Kj4yBGfpvQf85fQXJq5.sample" }}' \
+    > {temp_tsv} && \
+    tail -n +2 {intermediate_tsv} | \
+    awk '{{ print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10","$11"\t"$12","$13"\t"$14"\t"$15"\t"$16"\t"$17"\t"$1 }}' \
+    >> {temp_tsv}; tr -d '\r' < {temp_tsv} > {final_tsv}; \
+    rm {temp_tsv}
+    """.format(temp_tsv=temp_tsv, intermediate_tsv=intermediate_tsv, final_tsv=final_tsv)
+    
+    FNULL = open(os.devnull, 'w')
+    subprocess.call(command, stderr=subprocess.STDOUT, stdout=FNULL, shell=True)
+    assert os.path.exists(final_tsv), "Failed to generate batch file!"
+    return final_tsv
+
+def format_relative_paths(app_out_dirs):
+    result = ""
+    for stage, stage_info in sorted(workflow_stage_info.items()):
+        command_option = '--stage-relative-output-folder {stage_id} "{app_name}" '.format(stage_id=stage, app_name=stage_info["app_name"])
+        result += command_option
+    return result
+
+
+def run_dias_batch_file(workflow_id, batch_file, workflow_stage_info, workflow_out_dir):
+    app_relative_paths = format_relative_paths(workflow_stage_info)
+    command = 'dx run {workflow_id} --batch-tsv {batch_file} --destination={workflow_out_dir} {app_relative_paths}'\
+    	.format(workflow_id=workflow_id, batch_file=batch_file, workflow_out_dir=workflow_out_dir, app_relative_paths=app_relative_paths)
+    subprocess.call(command, shell=True)
+
+workflow_id         = "workflow-FpG6QjQ433Gf7Gq15ZF4Vk49"
 workflow_out_dir    = make_workflow_out_dir(workflow_id)
 workflow_stage_info = get_workflow_stage_info(workflow_id)
 app_out_dirs        = make_app_out_dirs(workflow_stage_info, workflow_id, workflow_out_dir)
+batch_file          = make_dias_batch_file()
+run_dias_batch_file(workflow_id, batch_file, workflow_stage_info, workflow_out_dir)
