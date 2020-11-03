ee0a96c8d84539140b049543e9992bf793f193c4
Yu-jinKim <yujin.kim@hotmail.fr>
2020-11-03 16:22:45 +0000

    loads of moving stuff around

--- multi_workflow.py
+++ multi_workflow.py
@@ -1,112 +1,28 @@
 #!/usr/bin/python
 
+from collections import OrderedDict
+import json
 import subprocess
-import uuid
 
 from config import (
-    stage_input_dict, ms_workflow_id, happy_stage_prefix
+    multi_stage_input_dict, ms_workflow_id, happy_stage_prefix
 )
 from general_functions import (
-    get_object_attribute_from_object_id_or_path,
-    get_date,
-    dx_make_workflow_dir,
     format_relative_paths,
     get_workflow_stage_info,
     make_app_out_dirs,
-    find_app_dir,
-    get_stage_input_file_list
+    fill_stage_input_dict,
+    make_dias_batch_file,
+    make_workflow_out_dir
 )
 
 # Multi sample apps
 
 
-def make_ms_workflow_out_dir(ms_workflow_id, ss_workflow_out_dir):
-    ms_workflow_name = get_object_attribute_from_object_id_or_path(
-        ms_workflow_id, "Name"
-    )
-    assert ms_workflow_name, "Workflow name not found. Aborting"
-    date = get_date()
-
-    i = 1
-
-    while i < 100:  # < 100 runs = sanity check
-        ms_workflow_output_dir = "{}{}-{}-{}/".format(
-            ss_workflow_out_dir, ms_workflow_name, date, i
-        )
-
-        if dx_make_workflow_dir(ms_workflow_output_dir):
-            print("Using\t\t%s" % ms_workflow_output_dir)
-            return ms_workflow_output_dir
-        else:
-            print("Skipping\t%s" % ms_workflow_output_dir)
-        i += 1
-    return None
-
-
-def get_ms_stage_input_dict(ss_workflow_out_dir):
-    for stage_input, stage_input_info in stage_input_dict.items():
-        # ss_workflow_out_dir = "/output/dias_v1.0.0_DEV-200430-1/"  # DEBUG
-        input_app_dir = find_app_dir(
-            ss_workflow_out_dir, stage_input_info["app"]
-        )
-        stage_input_dict[stage_input]["file_list"] = get_stage_input_file_list(
-            input_app_dir, app_subdir=stage_input_info["subdir"],
-            filename_pattern=stage_input_info["pattern"]
-        )
-
-    return stage_input_dict
-
-
-def make_ms_dias_batch_file(ms_stage_input_dict):
-    batch_uuid = str(uuid.uuid4())
-    batch_filename = ".".join([batch_uuid, "tsv"])
-
-    headers = ["batch ID"]
-    values = ["multi"]
-
-    # Hap.py - static values
-    headers.append(happy_stage_prefix)
-    values.append("NA12878")
-
-    # For each stage add the column header and the values in that column
-    for stage_input in ms_stage_input_dict:
-
-        if len(ms_stage_input_dict[stage_input]["file_list"]) == 0:
-            continue
-
-        headers.append(stage_input)  # col for file name
-        headers.append(" ".join([stage_input, "ID"]))  # col for file ID
-
-        # One file in file list - no need to merge into array
-        if len(ms_stage_input_dict[stage_input]["file_list"]) == 1:
-            file_ids = ms_stage_input_dict[stage_input]["file_list"][0]
-            values.append("")  # No need to provide file name in batch file
-            values.append(file_ids)
-
-        # make a square bracketed comma separated list if multiple input files
-        elif len(ms_stage_input_dict[stage_input]["file_list"]) > 1:
-            # Square bracketed csv list
-            file_id_list = [
-                file_id
-                for file_id in ms_stage_input_dict[stage_input]["file_list"]
-            ]
-            file_ids = "[{file_ids}]".format(file_ids=",".join(file_id_list))
-            values.append("")  # No need to provide file name in batch file
-            values.append(file_ids)
-
-    # Write the file content
-    with open(batch_filename, "w") as b_fh:
-        for line in [headers, values]:
-            tsv_line = "\t".join(line) + "\n"
-            b_fh.write(tsv_line)
-
-    return batch_filename
-
-
-def run_ms_workflow(ss_workflow_out_dir):
+def run_ms_workflow(ss_workflow_out_dir, dry_run):
     assert ss_workflow_out_dir.startswith("/"), (
         "Input directory must be full path (starting at /)")
-    ms_workflow_out_dir = make_ms_workflow_out_dir(
+    ms_workflow_out_dir = make_workflow_out_dir(
         ms_workflow_id, ss_workflow_out_dir
     )
     ms_workflow_stage_info = get_workflow_stage_info(ms_workflow_id)
@@ -114,10 +30,14 @@ def run_ms_workflow(ss_workflow_out_dir):
         ms_workflow_stage_info, ms_workflow_out_dir
     )
 
-    ms_stage_input_dict = get_ms_stage_input_dict(ss_workflow_out_dir)
-    ms_batch_file = make_ms_dias_batch_file(ms_stage_input_dict)
+    ms_stage_input_dict = fill_stage_input_dict(
+        ss_workflow_out_dir, multi_stage_input_dict
+    )
+    ms_batch_file = make_dias_batch_file(
+        ms_stage_input_dict, "multi", happy_stage_prefix
+    )
 
-    run_wf_command = "dx run --yes --ignore-reuse {} --batch-tsv={}".format(
+    run_wf_command = "dx run --yes --ignore-reuse --ignore-reuse-stage * {} --batch-tsv={}".format(
         ms_workflow_id, ms_batch_file
     )
 
@@ -126,6 +46,25 @@ def run_ms_workflow(ss_workflow_out_dir):
     destination = " --destination={} ".format(ms_workflow_out_dir)
 
     command = " ".join([run_wf_command, app_relative_paths, destination])
-    subprocess.check_call(command, shell=True)
+
+    if dry_run:
+        print("Created workflow dir: {}".format(ms_workflow_out_dir))
+        print("Stage info:")
+        print(json.dumps(OrderedDict(
+            sorted(ms_workflow_stage_info.iteritems())), indent=2)
+        )
+        print("Inputs gathered:")
+        print(json.dumps(ms_stage_input_dict, indent=4))
+        print("Created apps out dir: {}")
+        print(json.dumps(OrderedDict(
+            sorted(ms_output_dirs.iteritems())), indent=4)
+        )
+        print("Created batch tsv: {}".format(ms_batch_file))
+        print("Format of stage output dirs:")
+        print(app_relative_paths)
+        print("Final cmd ran:")
+        print(command)
+    else:
+        subprocess.call(command, shell=True)
 
     return ms_workflow_out_dir
