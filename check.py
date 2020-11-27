3476c03c90089ff614eea19866fee16e274b49bc
Yu-jinKim <yujin.kim@hotmail.fr>
2020-11-27 11:10:29 +0000

    docstring + no splitting the cmd + more info on bug

--- check.py
+++ check.py
@@ -6,6 +6,17 @@ from general_functions import (
 
 
 def check_if_all_reports_created(vcf2xls_dir, sample_sheet):
+    """ Return the difference between the samples in the sample sheet
+    the sample ids ending with _1.xls gathered from the vcf2xls directory
+
+    Args:
+        vcf2xls_dir (str): Directory in DNAnexus containing the initial batch of reports
+        sample_sheet (str): DNAnexus path to the samplesheet
+
+    Returns:
+        set: Set of samples ids missing from the report list compared to the sample sheet
+    """
+
     report_list = []
     sample_id_list = get_sample_ids_from_sample_sheet(sample_sheet)
     field, project_name = describe_object(get_dx_cwd_project_id())[3].split()
@@ -14,13 +25,16 @@ def check_if_all_reports_created(vcf2xls_dir, sample_sheet):
         "Debugging is needed"
     )
     print(project_name)
-    cmd = "dx ls {}".format(vcf2xls_dir).split()
-    list_of_created_reports = subprocess.check_output(cmd).split("\n")
+    cmd = "dx ls {}".format(vcf2xls_dir)
+    list_of_created_reports = subprocess.check_output(
+        cmd, shell=True
+    ).split("\n")
 
     # get reports generated in first iteration
     for report in list_of_created_reports:
         if report.endswith("_1.xls"):
-            # when i tried _1.xls, it would strip some of the numbers too
+            # when i tried _1.xls and the sample id ended with a 1
+            # it would strip the 1 too e.g. "X111111_1.xls".strip("_1.xls") become X
             # no idea why, can't be bothered to understand
             # am lazy
             report_list.append(report.strip("1.xls").strip("_"))
