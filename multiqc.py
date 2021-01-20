d7d7449511d6582246c2d424ff6c74609566d0c4
Yu-jinKim <yujin.kim@hotmail.fr>
2021-01-20 09:47:12 +0000

    multi folder optional when using multiqc

--- multiqc.py
+++ multiqc.py
@@ -6,9 +6,9 @@ from config import (
     mqc_applet_id,
     mqc_config_file
 )
+
 from general_functions import (
-    get_dx_cwd_project_id,
-    get_object_attribute_from_object_id_or_path,
+    get_dx_cwd_project_id, get_object_attribute_from_object_id_or_path,
     dx_make_workflow_dir
 )
 
@@ -21,11 +21,20 @@ def run_multiqc_app(ms_workflow_out_dir, dry_run):
 
     project_id = get_dx_cwd_project_id()
     path_dirs = [x for x in ms_workflow_out_dir.split("/") if x]
-    assert path_dirs[-3] == "output"
-    assert "single" in path_dirs[-2]
-    assert "multi" in path_dirs[-1]
-    ss_for_multiqc = path_dirs[-2]
-    ms_for_multiqc = path_dirs[-1]
+    ss_for_multiqc = [ele for ele in path_dirs if "single" in ele]
+    ms_for_multiqc = [ele for ele in path_dirs if "multi" in ele]
+
+    assert ss_for_multiqc != [], (
+        "Path '{}' is not an accepted directory, "
+        "must contain 'single'".format(ms_workflow_out_dir)
+    )
+
+    ss_for_multiqc = ss_for_multiqc[0]
+
+    if ms_for_multiqc != []:
+        multi_folder = "-ims_for_multiqc='{}'".format(ms_for_multiqc[0])
+    else:
+        multi_folder = ""
 
     mqc_applet_name = get_object_attribute_from_object_id_or_path(
         mqc_applet_id, "Name"
@@ -37,11 +46,11 @@ def run_multiqc_app(ms_workflow_out_dir, dry_run):
     command = (
         "dx run {} --yes --ignore-reuse -ieggd_multiqc_config_file='{}' "
         "-iproject_for_multiqc='{}' -iss_for_multiqc='{}' "
-        "-ims_for_multiqc='{}' --destination='{}'"
+        "{} --destination='{}'"
     ).format(
         mqc_applet_id, mqc_config_file,
         project_id, ss_for_multiqc,
-        ms_for_multiqc, mqc_applet_out_dir
+        multi_folder, mqc_applet_out_dir
     )
 
     if dry_run is True:
