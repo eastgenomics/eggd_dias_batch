eba0c5169f251e2b815462484ec921374e3a9a3f
Yu-jinKim <yujin.kim@hotmail.fr>
2021-06-08 11:56:21 +0100

    use semi colon to parse reanalysis files

--- reports.py
+++ reports.py
@@ -37,7 +37,7 @@ def run_reanalysis(input_dir, dry_run, assay_config, assay_id, reanalysis_list):
                 "sample/panel combination per line"
             )
             sample, panel = fields
-            panels = panel.split(",")
+            panels = panel.split(";")
 
             for panel in panels:
                 # get a dict of sample2panels
