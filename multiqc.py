e1511f2119281d9fd19ab8463f799104ca4b818f
Yujin Kim <yujin.kim@hotmail.fr>
2022-02-08 10:01:07 +0000

    use project name instead of project id for multiqc

--- multiqc.py
+++ multiqc.py
@@ -14,7 +14,7 @@ def run_multiqc_app(ms_workflow_out_dir, dry_run, assay_config):
     assert ms_workflow_out_dir.startswith("/"), (
         "Input directory must be full path (starting at /)")
 
-    project_id = get_dx_cwd_project_id()
+    project_name = get_dx_cwd_project_name()
     path_dirs = [x for x in ms_workflow_out_dir.split("/") if x]
     ss_for_multiqc = [ele for ele in path_dirs if "single" in ele]
     ms_for_multiqc = [ele for ele in path_dirs if "multi" in ele]
@@ -44,8 +44,7 @@ def run_multiqc_app(ms_workflow_out_dir, dry_run, assay_config):
         "{} --destination='{}'"
     ).format(
         assay_config.mqc_applet_id, assay_config.mqc_config_file,
-        project_id, ss_for_multiqc,
-        multi_folder, mqc_applet_out_dir
+        project_name, ss_for_multiqc, multi_folder, mqc_applet_out_dir
     )
 
     if dry_run is True:
