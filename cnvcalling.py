c679c96390e5fac931eb5fb2469d90ff26cc88b9
AishaD <AishaDahir75@gmail.com>
2022-08-22 12:06:22 +0100

    GCNV-15 print command

--- cnvcalling.py
+++ cnvcalling.py
@@ -146,6 +146,7 @@ def run_cnvcall_app(ss_workflow_out_dir, dry_run, assay_config, assay_id, sample
         file_ids,
         project_name, app_output_dir
     )
+    print(command)
 
     if dry_run is True:
         print("Final cmd ran: {}".format(command))
