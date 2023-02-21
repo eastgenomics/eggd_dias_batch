a4cbed1f1e32a5566a6e64094d7d4b700b80b2c3
sophie22 <rzsozso@gmail.com>
2023-02-21 11:39:41 +0000

    minor wording and order changes

--- dias.py
+++ dias.py
@@ -4,13 +4,13 @@ import argparse
 import imp
 import os
 
+from general_functions import get_latest_config
 from single_workflow import run_ss_workflow
 from multi_workflow import run_ms_workflow
 from multiqc import run_multiqc_app
 from cnvcalling import run_cnvcall_app
-from cnvreports import run_cnvreports, run_cnvreanalysis
 from reports import run_reports, run_reanalysis
-from general_functions import get_latest_config
+from cnvreports import run_cnvreports, run_cnvreanalysis
 
 
 TSOE_CONFIG_LOCATION = "/mnt/storage/apps/software/egg1_dias_TSO_config"
@@ -34,12 +34,12 @@ def main():
         )
     )
     parser.add_argument(
-        "-c", "--config", help="Config file to overwrite the config assay setup"
+        "-c", "--config", help="Config file to overwrite the assay setup"
     )
 
     parser_s = subparsers.add_parser('single', help='single help')
     parser_s.add_argument(
-        'input_dir', type=str, help='Input data directory path'
+        'input_dir', type=str, help='Sequencing data (FASTQ) directory path'
     )
     parser_s.set_defaults(which='single')
 
@@ -71,27 +71,6 @@ def main():
     )
     parser_n.set_defaults(which='cnvcall')
 
-    parser_r = subparsers.add_parser('cnvreports', help='cnvreports help')
-    parser_r.add_argument(
-        'input_dir', type=str,
-        help='A single/multi sample workflow output directory path'
-    )
-    parser_r.set_defaults(which='cnvreports')
-
-    parser_r = subparsers.add_parser('cnvreanalysis', help='cnvreanalysis help')
-    parser_r.add_argument(
-        'input_dir', type=str,
-        help='A single/multi sample workflow output directory path'
-    )
-    parser_r.add_argument(
-        'cnvreanalysis_list', type=str,
-        help=(
-            'Tab delimited file containing sample and panel for cnvreanalysis'
-            '. One sample/panel combination per line'
-        )
-    )
-    parser_r.set_defaults(which='cnvreanalysis')
-
     parser_r = subparsers.add_parser('reports', help='reports help')
     parser_r.add_argument(
         'input_dir', type=str,
@@ -113,11 +92,33 @@ def main():
     )
     parser_r.set_defaults(which='reanalysis')
 
+    parser_r = subparsers.add_parser('cnvreports', help='cnvreports help')
+    parser_r.add_argument(
+        'input_dir', type=str,
+        help='A single/multi sample workflow output directory path'
+    )
+    parser_r.set_defaults(which='cnvreports')
+
+    parser_r = subparsers.add_parser('cnvreanalysis', help='cnvreanalysis help')
+    parser_r.add_argument(
+        'input_dir', type=str,
+        help='A single/multi sample workflow output directory path'
+    )
+    parser_r.add_argument(
+        'cnvreanalysis_list', type=str,
+        help=(
+            'Tab delimited file containing sample and panel for cnvreanalysis'
+            '. One sample/panel combination per line'
+        )
+    )
+    parser_r.set_defaults(which='cnvreanalysis')
+
     args = parser.parse_args()
     workflow = args.which
 
     assert workflow, "Please specify a subcommand"
 
+# could use yaml config files and then use yaml.safe_load()
     if args.config:
         assay_id = "CUSTOM_CONFIG"
         name_config = os.path.splitext(args.config)[0]
@@ -126,21 +127,21 @@ def main():
         if args.assay == "TSOE":
             latest_version = get_latest_config(TSOE_CONFIG_LOCATION)
             config = imp.load_source(
-                "egg1_config", "{}/{}/egg1_config.py".format(
+                "egg1_config", "{}/{}/egg1_config*.py".format(
                     TSOE_CONFIG_LOCATION, latest_version
                 )
             )
         elif args.assay == "FH":
             latest_version = get_latest_config(FH_CONFIG_LOCATION)
             config = imp.load_source(
-                "egg3_config", "{}/{}/egg3_config.py".format(
+                "egg3_config", "{}/{}/egg3_config*.py".format(
                     FH_CONFIG_LOCATION, latest_version
                 )
             )
         elif args.assay == "TWE":
             latest_version = get_latest_config(TWE_CONFIG_LOCATION)
             config = imp.load_source(
-                "egg4_config", "{}/{}/egg4_config.py".format(
+                "egg4_config", "{}/{}/egg4_config*.py".format(
                     TWE_CONFIG_LOCATION, latest_version
                 )
             )
