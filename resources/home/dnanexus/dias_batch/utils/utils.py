8a997539edd500b4f565d650f7773f41b43d0420
Jethro Rainford <rainford1995@gmail.com>
2023-09-12 15:01:47 +0100

    add make_path() to bodge our own dx paths

--- resources/home/dnanexus/dias_batch/utils/utils.py
+++ resources/home/dnanexus/dias_batch/utils/utils.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import re
+
 def time_stamp() -> str:
     """
     Returns string of date & time formatted as YYMMDD_HHMM
@@ -8,3 +11,25 @@ def time_stamp() -> str:
         String of current date and time as YYMMDD_HHMM
     """
     return datetime.now().strftime("%y%m%d_%H%M")
+
+def make_path(*path) -> str:
+    """
+    Generate path-like string (i.e. for searching in DNAnexus) or
+    when setting output folder for running an app
+
+    Parameters
+    ----------
+    path : iterable
+        strings of path parts to join
+
+    Returns
+    -------
+    str
+        nicely formated path with leading and trailing forward slash
+    """
+    path = '/'.join([
+        re.sub(r"project-[\d\w]+:", "", x).lstrip('/').rstrip('/')
+        for x in path if x
+    ])
+
+    return f"/{path}/"
