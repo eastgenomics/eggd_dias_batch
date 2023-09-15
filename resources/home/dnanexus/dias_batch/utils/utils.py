03bfc6b40767f51ac3fc023233ca915b91d23151
Jethro Rainford <rainford1995@gmail.com>
2023-09-15 14:59:15 +0100

    add function to filter samples by available sample files

--- resources/home/dnanexus/dias_batch/utils/utils.py
+++ resources/home/dnanexus/dias_batch/utils/utils.py
@@ -57,7 +57,8 @@ def parse_manifest(contents, split_tests=False) -> pd.DataFrame:
     Returns
     -------
     dict
-        mapping od sampleID (str) : testCodes (list)
+        mapping of sampleID (str): 'tests': testCodes (list)
+        e.g. {'sample1': {'tests': [['panel1']]}}
     
     Raises
     ------
@@ -70,6 +71,12 @@ def parse_manifest(contents, split_tests=False) -> pd.DataFrame:
         "Parsing manifest file, file contents read from DNAnexus:\n\t",
         "\n\t".join(contents)
     )
+
+    # turn df into a dict mapping sample ID to list of test codes,
+    # duplicate samples in the same manifest will result in >1 list
+    # of test codes, will be structured as:
+    # {'sample1': {'tests': [['panel1', 'gene1'], ['panel2']]}}
+    data = defaultdict(lambda: defaultdict(list))
 
     if all(';' in x for x in contents[1:] if x):
         # csv file => Epic style manifest
@@ -90,10 +97,6 @@ def parse_manifest(contents, split_tests=False) -> pd.DataFrame:
         manifest = manifest[['SampleID', 'ReanalysisID', 'Test Codes']]
         manifest_source = 'Epic'
 
-        # turn df into a dict mapping sample ID to list of test codes,
-        # duplicate samples in the same manifest will result in >1 list
-        # of test codes
-        data = defaultdict(list)
         for idx, row in manifest.iterrows():
             # split test codes to list and sense check they're valid format
             # will be formatted as 'R211.1, , , ,' or '_HGNC:1234, , , ,' etc.
@@ -109,9 +112,9 @@ def parse_manifest(contents, split_tests=False) -> pd.DataFrame:
 
             # prefentially use ReanalysisID if present
             if re.match(r"[\d\w]+-[\d\w]+", row.ReanalysisID):
-                data[row.ReanalysisID].append(test_codes)
+                data[row.ReanalysisID]['tests'].append(test_codes)
             elif re.match(r"[\d\w]+-[\d\w]+", row.SampleID):
-                data[row.SampleID].append(test_codes)
+                data[row.SampleID]['tests'].append(test_codes)
             else:
                 # some funky with this sample naming
                 raise RuntimeError(
@@ -138,7 +141,7 @@ def parse_manifest(contents, split_tests=False) -> pd.DataFrame:
                     'Invalid test code provided for sample '
                     f'{sample[0]} : {sample[1]}'
                 )
-            data[sample[0]].append(sample[1])
+            data[sample[0]]['tests'].append(sample[1])
         
         manifest_source = 'Gemini'
 
@@ -148,10 +151,108 @@ def parse_manifest(contents, split_tests=False) -> pd.DataFrame:
             f"Manifest file provided does not seem valid"
         )
 
-    samples = ('\n\t').join([f"{x[0]} -> {x[1]}" for x in data.items()])
+    samples = ('\n\t').join([
+        f"{x[0]} -> {x[1]['tests']}" for x in data.items()
+    ])
     print(f"{manifest_source} manifest parsed:\n\t{samples}")
 
     return data
+
+
+def filter_manifest_samples_by_files(manifest, files, pattern) -> dict:
+    """
+    Filter samples in manifest against those where required per sample
+    files have been found with DXManage.find_files().
+
+    Used where there may be required per sample files missing for a given
+    sample (i.e. .sample has failed or explicitly been excluded from running)
+
+    Parameters
+    ----------
+    manifest : dict
+        dict mapping sampleID -> testCodes from parse_manifest()
+    files : list
+        list of DXFile objects returned from DXMange.find_files()
+    pattern : str
+        regex pattern for selecting parts of name to match on, i.e.
+            (Gemini naming)
+            manifest name : X12345
+            vcf name      : X12345-GM12345_much_suffix.vcf.gz
+            pattern       : '^X[\d]+'
+
+            (Epic naming)
+            manifest_name : 124801362-23230R0131
+            vcf name      : 124801362-23230R0131-23NGSCEN15-8128-M-96527.vcf.gz
+            pattern       : '^[\d\w]+-[\d\w]+'
+
+
+    Returns
+    -------
+    dict
+        subset of manifest mapping dict with samples removed that have
+        no files and with DXFile objects added under 'files' as a list
+        for each sample where one or more files were found
+    """
+    # build mapping of prefix using given pattern to matching files
+    file_prefixes = defaultdict(list)
+    for file in files:
+        match = re.match(pattern, file['describe']['name'])
+        if match:
+            file_prefixes[match.group()].append(file)
+
+    manifest_no_match = []
+    manifest_no_files = []
+
+    for sample in manifest.keys():
+        match = re.match(pattern, sample)
+        if not match:
+            # sample ID doesn't match expected pattern
+            manifest_no_match.append(sample)
+            manifest.pop(sample)
+        else:
+            # we have prefix, try find matching files with same prefix
+            sample_files = file_prefixes.get(match.group())
+            if not sample_files:
+                # found no files for this sample
+                print(
+                    f"No files found for {sample} using pattern {pattern}, "
+                    f"prefix matched in samplename {match.group()}"
+                )
+                manifest_no_files.append(sample)
+                manifest.pop(sample)
+            else:
+                print(
+                    f"Found {len(sample_files)} files for {sample}\n"
+                    f"{[x['describe']['name'] for x in sample_files]}"
+                )
+                manifest[sample]['files'] = sample_files
+    
+    print(
+        f"{len(manifest_no_match)} samples in manifest didn't match expected"
+        f"pattern of {pattern}: {manifest_no_match}"
+    )
+
+    print(
+        f"{len(manifest_no_files)} samples in manifest didn't have any "
+        f"matching files: {manifest_no_files}"
+    )
+
+    return manifest
+
+
+
+                
+
+
+
+
+    manifest_mapping = {
+        re.match(pattern, k).group(): k
+        if re.match(pattern, k) else None
+        for k, v in manifest
+    }
+
+
 
 def split_tests(data) -> dict:
     """
@@ -159,17 +260,17 @@ def split_tests(data) -> dict:
     instead of being combined
 
     Data structure before will be some form of: {
-        "sample1" : [['panel1', 'panel2', '_gene1']],
-        "sample2" : [['panel3']],
-        "sample3" : [['panel1'], ['panel2', 'gene2', 'gene3']]
-        "sample4" : [['panel1' 'gene1'], ['panel2', 'gene2']]
+        "sample1" : {'tests': [['panel1', 'panel2', '_gene1']]},
+        "sample2" : {'tests': [['panel3']]},
+        "sample3" : {'tests': [['panel1'], ['panel2', 'gene2', 'gene3']]},
+        "sample4" : {'tests': [['panel1' 'gene1'], ['panel2', 'gene2']]}
     }
 
     which will change to: {
-        "sample1" : [['panel1'], ['panel2'], ['_gene1']],
-        "sample2" : [['panel3']],
-        "sample3" : [['panel1'], ['panel2'], ['gene2', 'gene3']],
-        "sample4" : [['panel1'], ['gene1'], ['panel2'], ['gene2']]
+        "sample1" : {'tests': [['panel1'], ['panel2'], ['_gene1']]},
+        "sample2" : {'tests': [['panel3']]},
+        "sample3" : {'tests': [['panel1'], ['panel2'], ['gene2', 'gene3']]},
+        "sample4" : {'tests': [['panel1'], ['gene1'], ['panel2'], ['gene2']]}
     }
 
     n.b. genes in the same initial sub list will always be grouped together
@@ -186,12 +287,12 @@ def split_tests(data) -> dict:
     Returns
     -------
     dict
-        mapping of SampleID : [testCodes] with all codes are sub lists
+        mapping of SampleID: 'tests': [testCodes] with all codes are sub lists
     """
-    split_data = defaultdict(list)
+    split_data = defaultdict(lambda: defaultdict(list))
     for sample, test_codes in data.items():
         all_split_test_codes = []
-        for test_list in test_codes:
+        for test_list in test_codes['tests']:
             test_genes = []
             for idx, sub_test in enumerate(test_list):
                 if re.match(r"[RC][\d]+\.[\d]+", sub_test):
@@ -204,7 +305,7 @@ def split_tests(data) -> dict:
                 # there were some single genes to test
                 all_split_test_codes.append(list(set(test_genes)))
         
-        split_data[sample].extend(all_split_test_codes)
+        split_data[sample]['tests'].extend(all_split_test_codes)
     
     return split_data
 
