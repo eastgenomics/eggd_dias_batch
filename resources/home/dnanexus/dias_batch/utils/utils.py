3dd2b7a59fdd7a63f43b66de272dd026bd34dc35
Jethro Rainford <rainford1995@gmail.com>
2023-09-15 13:24:59 +0100

    fix typos

--- resources/home/dnanexus/dias_batch/utils/utils.py
+++ resources/home/dnanexus/dias_batch/utils/utils.py
@@ -79,8 +79,8 @@ def parse_manifest(contents, split_tests=False) -> pd.DataFrame:
         contents = [x.split(';') for x in contents if x]
         manifest = pd.DataFrame(contents[2:], columns=contents[1])
 
-        # sample id may be split between 'Sepcimen ID' and 'Instrument ID' or
-        # Re-analysis Specimen ID and Re-analysis Instrument ID olumns, join
+        # sample id may be split between 'Specimen ID' and 'Instrument ID' or
+        # Re-analysis Specimen ID and Re-analysis Instrument ID columns, join
         # these as {InstrumentID-SpecimenID} to get a mapping of sample ID -> CI
         manifest['SampleID'] = manifest['Instrument ID'] + \
             '-' + manifest['Specimen ID']
